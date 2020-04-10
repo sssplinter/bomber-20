a98b3531f323c2be5d0728e56d816bfd7a81f7cc
Kristina <kristinasementsova21@gmaol.com>
2020-04-11 01:34:17 +0300

    'create_constants'

---
+++ src/game/Constants.java
@@ -0,0 +1,10 @@
+package game;
+
+public class Constants {
+    static final int BLOCK_SIZE = 64;
+    static final int CHARACTER_SIZE = 32;
+    static final int ENEMY_SIZE = 32;
+
+    static final int CHARACTER_SPEED = 64;
+
+}
