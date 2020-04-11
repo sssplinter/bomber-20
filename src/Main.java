9836004a1d3d4ea910bda570ddf155c5daf7b509
Kristina <kristinasementsova21@gmaol.com>
2020-04-12 01:03:26 +0300

    'create_main'

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainMenu/mainMenuScene.fxml"));
+        Parent root = (Parent) loader.load();
+        Stage stage= new Stage();
+        stage.setScene(new Scene(root, 640, 780));
+        stage.showAndWait();
     }
 }
--- src/mainMenu/MainMenuController.java
+++ src/mainMenu/MainMenuController.java
@@ -1,4 +1,21 @@
 package mainMenu;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainMenuController {
+
+    @FXML
+    public void onNewGamePressed() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../levelMenu/levelMenu.fxml"));
+        Parent root = (Parent) loader.load();
+        Stage stage= new Stage();
+        stage.setScene(new Scene(root, 640, 780));
+        stage.showAndWait();
+    }
 }
