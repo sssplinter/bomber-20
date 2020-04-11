a19c5b9d93ddb6e3c6252c2e0115b178e586e70e
Kristina <kristinasementsova21@gmaol.com>
2020-04-11 03:08:07 +0300

    'create_level_content'

---
+++ src/game/LevelData.java
@@ -0,0 +1,19 @@
+package game;
+
+public class LevelData {
+
+    //TODO  заменить на матрицу блоков
+    private int[][] levelContent;
+    private int[][] enemyLocation;
+
+    private int width ;
+    private int height;
+
+    public int getWidth(){
+       return width;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+}
