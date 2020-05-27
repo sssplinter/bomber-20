78701ff55a797ac4bca6db28a9f2bc8f68a94c1f
Kristina <kristinasementsova21@gmaol.com>
2020-05-28 01:13:46 +0300

    add new block type to block factory

--- src/game/blocks/BlockFactory.java
+++ src/game/blocks/BlockFactory.java
@@ -7,11 +7,13 @@ public class BlockFactory {
     private static final int BACKGROUND_CODE = 0;
     private static final int BREAKABLE_CODE = 1;
     private static final int BORDER_CODE = 2;
+    private static final int ENEMY_CODE = 3;
 
     @NotNull
     public Block createBlock(final int blockCode) {
         switch (blockCode) {
-            case BACKGROUND_CODE:
+            case BACKGROUND_CODE: //линейное движение
+            case ENEMY_CODE:
                 return new BackgroundBlock();
             case BREAKABLE_CODE:
                 return new BreakableBlock();
@@ -20,5 +22,9 @@ public class BlockFactory {
             default:
                 throw new IllegalArgumentException("Illegal block code.");
         }
+    }
+
+    public boolean isEnemy(final int code){
+        return code == ENEMY_CODE;
     }
 }
