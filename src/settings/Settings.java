d35ebd1356ed7c65ed5d2bcabc3aa1bd07d6cb07
Kristina <kristinasementsova21@gmaol.com>
2020-05-29 20:40:43 +0300

    create settings

--- src/settings/Settings.java
+++ src/settings/Settings.java
@@ -1,4 +1,23 @@
 package settings;
 
-public class settings {
+public class Settings {
+
+    private static final Object lock = new Object();
+    private static Settings instance;
+
+    private boolean musicEnabled = true;
+
+    private Settings() {
+    }
+
+    public static Settings getInstance() {
+        if (instance == null) {
+            synchronized (lock) {
+                if (instance == null) {
+                    instance = new Settings();
+                }
+            }
+        }
+        return instance;
+    }
 }
