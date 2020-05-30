e3eaebcaed3a015aa2d9b68f8b861aa5f17554fc
Kristina <kristinasementsova21@gmaol.com>
2020-05-30 13:09:22 +0300

    add enemy move

--- src/game/characters/Enemy.java
+++ src/game/characters/Enemy.java
@@ -3,10 +3,13 @@ package game.characters;
 import game.Constants;
 import game.LevelData;
 
+import java.util.Random;
+
 public class Enemy extends Character {
 
     public Enemy(LevelData levelData) {
         super(levelData);
+        new MoveThread().start();
     }
 
     @Override
@@ -23,5 +26,44 @@ public class Enemy extends Character {
     protected String getImagePath() {
         return Constants.CHARACTER_IMAGE;
     }
-}
 
+    private class MoveThread extends Thread {
+
+        private static final int UP = 0;
+        private static final int DOWN = 1;
+        private static final int LEFT = 2;
+        private static final int RIGHT = 3;
+
+        private Random random = new Random();
+
+        @Override
+        public void run() {
+            while (Enemy.this.isAlive()) {
+                final int direction = Math.abs(random.nextInt()) % 4;
+                for (int i = 0; i < 6; i++) {
+                    switch (direction) {
+                        case UP:
+                            Enemy.this.moveUp();
+                            break;
+                        case DOWN:
+                            Enemy.this.moveDown();
+                            break;
+                        case LEFT:
+                            Enemy.this.moveLeft();
+                            break;
+                        case RIGHT:
+                            Enemy.this.moveRight();
+                            break;
+                        default:
+                            throw new IllegalArgumentException();
+                    }
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
