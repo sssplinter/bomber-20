0f4ef8e0a0b7b0aab81077156be545380face989
Kristina <kristinasementsova21@gmaol.com>
2020-05-31 12:44:19 +0300

    create back in complexityMenu

--- src/complexityMenu/ComplexityMenuController.java
+++ src/complexityMenu/ComplexityMenuController.java
@@ -5,9 +5,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import utilities.StageUtils;
 
 import java.io.IOException;
 
@@ -48,6 +51,11 @@ public class ComplexityMenuController {
     @FXML
     ImageView bomb6;
 
+    @FXML
+    Button levelMenu;
+
+    @FXML
+    ImageView backImg;
 
     private Image easy;
     private Image selEasy;
@@ -55,6 +63,8 @@ public class ComplexityMenuController {
     private Image selNormal;
     private Image hard;
     private Image selHard;
+    private Image back;
+    private Image selBack;
 
 
     @FXML
@@ -65,16 +75,21 @@ public class ComplexityMenuController {
         selNormal = new Image(Constants.SELECTED_NORMAL_IMAGE);
         hard = new Image(Constants.HARD_IMAGE);
         selHard = new Image(Constants.SELECTED_HARD_IMAGE);
+        back = new Image(Constants.BACK_TO_LEVEL_MENU);
+        selBack = new Image(Constants.SEL_BACK_TO_LEVEL_MENU);
     }
 
     @FXML
     public void onEasyGamePressed() throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../testLevelMenu/levelMenuScene.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../levelMenu/levelMenuScene.fxml"));
         Parent root = (Parent) loader.load();
         Stage stage = new Stage();
         stage.setTitle("BOMBERMEN");
         stage.setScene(new Scene(root, 640, 480));
-        stage.showAndWait();
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.show();
+        StageUtils.closeStage(pointer2);
+
     }
 
     @FXML
@@ -83,11 +98,23 @@ public class ComplexityMenuController {
     }
 
     @FXML
+    public void onBackToLevelMenu() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../mainMenu/mainMenuScene.fxml"));
+        Parent root = (Parent) loader.load();
+        Stage stage= new Stage();
+        stage.setTitle("BOMBERMEN");
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setScene(new Scene(root, 620, 480));
+        stage.show();
+        StageUtils.closeStage(pointer2);
+    }
+
+    @FXML
     public void onHardGamePressed() {
 
     }
 
-    public void setInvisible(){
+    private void setInvisible(){
         bomb1.setVisible(false);
         bomb2.setVisible(false);
         bomb3.setVisible(false);
@@ -96,13 +123,21 @@ public class ComplexityMenuController {
         bomb6.setVisible(false);
     }
 
-    @FXML
-    public void onEasyEntered() {
+    private void setUnselected() {
         setInvisible();
+        pointer1.setVisible(false);
+        easyGame.setImage(easy);
         pointer2.setVisible(false);
         normalGame.setImage(normal);
         pointer3.setVisible(false);
         hardGame.setImage(hard);
+        backImg.setImage(back);
+    }
+
+
+    @FXML
+    public void onEasyEntered() {
+        setUnselected();
         pointer1.setVisible(true);
         easyGame.setImage(selEasy);
         bomb1.setVisible(true);
@@ -110,11 +145,7 @@ public class ComplexityMenuController {
 
     @FXML
     public void onNormalEntered() {
-        setInvisible();
-        pointer1.setVisible(false);
-        easyGame.setImage(easy);
-        pointer3.setVisible(false);
-        hardGame.setImage(hard);
+        setUnselected();
         pointer2.setVisible(true);
         normalGame.setImage(selNormal);
         bomb2.setVisible(true);
@@ -123,15 +154,17 @@ public class ComplexityMenuController {
 
     @FXML
     public void onHardEntered() {
-        setInvisible();
-        pointer1.setVisible(false);
-        easyGame.setImage(easy);
-        pointer2.setVisible(false);
-        normalGame.setImage(normal);
+        setUnselected();
         pointer3.setVisible(true);
         hardGame.setImage(selHard);
         bomb4.setVisible(true);
         bomb5.setVisible(true);
         bomb6.setVisible(true);
+    }
+
+    @FXML
+    public void onBackEntered(){
+        setUnselected();
+        backImg.setImage(selBack);
     }
 }
